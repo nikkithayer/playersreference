3a8ba34cdd236f0873efa6ea80385005bb287628
Nicole Thayer <nicole@carbonfive.com>
2021-06-26 06:55:42 -0700

    Got notes looping ok

--- application/views/read-list.php
+++ application/views/read-list.php
@@ -1,64 +1,87 @@
 <?php
 
 // Attempt select query execution
-$sql_unit = "SELECT * FROM $playUnits";
+$sql = "SELECT * FROM $playUnits LEFT JOIN $playContents ON $playUnits.unitId = $playContents.unitId LEFT JOIN $playNotes ON $playContents.lineId = $playNotes.lineId";
+if($result = mysqli_query($link, $sql)){
+  if(mysqli_num_rows($result) > 0){
+    $lastAct = "";
+    $lastUnit = "";
+    $lastLine = "";
+    $notesArray = array();
+    while($row = mysqli_fetch_array($result)){
+      if($row['act']!=$lastAct){
+        echo "<h1>" . $row['act'] . "</h1>";
+        $lastAct = $row['act'];
+      }
 
-if($result_unit = mysqli_query($link, $sql_unit)){
-    if(mysqli_num_rows($result_unit) > 0){
-
-      echo "<div class='unit'>";
-
-        while($row = mysqli_fetch_array($result_unit)){
-            echo "<div class='unit-meta'>";
-            echo "<h1>" . $row['act'] . "</h1>";
-            echo "<h2>" . $row['unit'] ." ". $row['unitTitle']."</h2>";
-            echo "<h3>" . $row['unitLocation'] . "</h3>";
-            echo "<h3>" . $row['unitDescription'] . "</h3>";
-            echo "</div>";
-
-            $sql_line = "SELECT * FROM $playContents WHERE unitID=$row[unitId]";
-            if($result_line = mysqli_query($link, $sql_line)){
-              if(mysqli_num_rows($result_line) > 0){
-                while($row_line = mysqli_fetch_array($result_line)){
-                  echo "<div class='".$row_line['lineType']."' id='line-".$row_line['lineId']."'>".$row_line['content'] . "</div>";
-
-                  $sql_note = "SELECT * FROM $playNotes WHERE lineID=$row_line[lineId]";
-                  if($result_note = mysqli_query($link, $sql_note)){
-                    if(mysqli_num_rows($result_note) > 0){
-                      while($row_note = mysqli_fetch_array($result_note)){
-                        echo $row_note['noteId'];
-                        echo $row_note['lineId'];
-                        echo $row_note['contentString'];
-                        echo $row_note['noteType'];
-                        echo $row_note['scansionAlt'];
-                        echo $row_note['noteContent'];
-                      }
-                      mysqli_free_result($result_note);
-                    }
-                  } else{
-                      echo "ERROR: Could not able to execute $sql_note. " . mysqli_error($link);
-                  }
-
-                }
-                mysqli_free_result($result_line);
-              } else{
-                  echo "No records matching your query were found.";
-              }
-            } else{
-                echo "ERROR: Could not able to execute $sql_line. " . mysqli_error($link);
-            }
-        }
+      if($row['unitId']!==$lastUnit){
+        echo "<div class='unit-meta'>";
+        echo "<h2>" . $row['unit'] . " " . $row['unitTitle'] . "</h2>";
+        echo "<h3>" . $row['unitLocation'] . "</h3>";
+        echo "<h3>" . $row['unitDescription'] . "</h3>";
+ //       echo $row['sceneBreak'];
         echo "</div>";
-        // Free result set
-        mysqli_free_result($result_unit);
-    } else{
-        echo "No records matching your query were found.";
+        $lastUnit = $row['unitId'];
+      }
+
+// here's a line that's different from the last line
+      if($row['content']!==$lastLine){
+// print the last line with all the modifications, then
+        echo $lastLine;
+        foreach($notesArray as $test){
+          echo $test."<br>";
+        }
+        unset($notesArray);
+// does it have a note? no - don't do anything and it will print in the next cycle
+// does it have a note? yes - tight, add the note to a new array
+        if($row['contentString']!==""){
+          $notesArray[] = $row['contentString'];
+        }
+// here's a line that's the same
+      }else{
+// ah neat, it has a note then. add the note to the array
+        $notesArray[] = $row['contentString'];
+      }
+      $lastLine = $row['content'];
+
+/*
+//        echo "<div class='".$row['lineType']." ".$row['lineTags']."' id='line-".$row['lineId']."'>".$row['content'] . "</div>";
+      if($row['contentString']!==""){
+        addNotes($row['content'],$row['contentString']);
+        echo $row['contentString'];
+        echo "<br>";
+        echo $row['noteType'];
+        echo "<br>";
+        echo $row['scansionAlt'];
+        echo "<br>";
+        echo $row['noteContent'];
+        echo "<br>";      
+      }
+*/
     }
+    mysqli_free_result($result);
+    echo "</table>";
+  }
 } else{
-    echo "ERROR: Could not able to execute $sql_unit. " . mysqli_error($link);
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
 }
 
 // Close connection
 mysqli_close($link);
+
+function addNotes($line, $string){
+  if (strpos($line, $string)!== false) {
+    //break into array at string position and string position + string length
+    echo strpos($line, $string);
+    echo strlen($string);
+  }
+}
+
+//take all notes and order by string position
+//if two notes have the same string position, start with the one with the longer string length
+// if the content string for a note exists
+// take the contents of the previous line and break them into an array
+// then wrap the contents of the matched string in the right tag
+// then convert the array back into a string
 
 ?>
